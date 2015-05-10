d4436c75ebcc236304ff1ba1b321e1961d04b163
cameronkent <cameronkent@Camerons-MacBook-Air-2.local>
2015-05-10 10:35:25 +1000

    FIXED STUFF
    fixed banner image
    added bulletin to nag bar
    changed icons

--- cameronsWork/contact.php
+++ cameronsWork/contact.php
@@ -11,12 +11,12 @@
         <div class="container">
             <div id="navigation" class="row col">
                 <ul>
-                    <li><img src="images/disc.png" class="navIcon"><a href="index.php" class="">Home </a></li>
-                    <li><img src="images/speaker.png" class="navIcon"><a href="events.php" class="">Events </a></li>
-                    <li><img src="images/deck.png" class="navIcon" ><a href="artists.php" class="">Artists </a></li>
-                    <li><img src="images/sheet.png" class="navIcon"><a href="members.php" class="">Members </a></li>
-                    <li><img src="images/mic.png" class="navIcon"><a href="contact.php" class="">Contact </a></li>
-                </ul>
+                    <li><img src="images/navIcons/home.gif" class="navIcon"><a href="index.php" class="">Home </a></li>
+                    <li><img src="images/navIcons/events.gif" class="navIcon"><a href="events.php" class="">Events </a></li>
+                    <li><img src="images/navIcons/artists.gif" class="navIcon" ><a href="artists.php" class="">Artists </a></li>
+                    <li><img src="images/navIcons/members.gif" class="navIcon"><a href="members.php" class="">Members </a></li>
+                    <li><img src="images/navIcons/bulletins.gif" class="navIcon"><a href="bulletins.php" class="">Bulletins </a></li>
+                    <li><img src="images/navIcons/contact.gif" class="navIcon"><a href="contact.php" class="">Contact </a></li>
             </div>
         </div>
     </div><div class="container">
