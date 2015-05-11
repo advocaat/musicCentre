d411f230ea8806eb2d821f23a3b4f931fb5fe086
Daan97 <daan.feltonbusch@my.jcu.edu.au>
2015-05-11 10:45:10 +1000

    bulletins html
    updating html for bulletins

--- cameronsWork/bulletins.php
+++ cameronsWork/bulletins.php
@@ -26,222 +26,26 @@
                 <h2>Community Bulletins</h2>
                 <hr>
                 <!-- DAAN put bulletin html in here mkay -->
-                 <!--Main Content-->
-    <div class="container">
-        <h1 class="font-center">Welcome to our Bulletin Board!</h1>
-        <div class="col-wide">
-        	<div class="boxed"> 
-	    
-     <!--St Joseph's-->
-<div class="boxed">
-  
-
-<img src="TCMC/stjosephs.PNG" width="382" height="180" alt=""/> 
-<table class="BB">
-    <thead>
-            	<tr>
-                	<th colspan="5">Volunteer Singers / Musicians<br>
-               	    <br></th>
+                
+                
+                 <div class="BulletinItem">
+                	
+                  <img src="bulletins images/stjosephs.PNG" width="382" height="180" alt=""/>  
+                    <h2>Volunteer Singers / Musicians</h2>
+               	    
                     
-                </tr>
-            </thead>
-            <tbody>
- 				<td>Our Parish Priest, Fr Mick Peters, is trying to development and foster a community for our 6 PM Vigil Mass at St Josephs on the Strand. 
+               <p> Our Parish Priest, Fr Mick Peters, is trying to development and foster a community for our 6 PM Vigil Mass at St Josephs on the Strand. 
 I'm helping by organising an event format, where I invite our members to bring a plate for a buffet and I provide music, or friends who can sing or perform. 
 Now that we are achieving some success, we need more people who can sing or play music. 
 I'm open to any kind of singing or performing so long as it is not too controversial. My preference would be for an artist to sing a medley of old time favorites. 
 Mass starts at 6pm and finishes about 10 to 7.
-If you would like to take part, please call Merle Trembath 0418882633 anytime. 
-Posted 24-Mar-15 </td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-            </tfoot>
-            </table>
-<br>
-</div>
-	
-    <!--Call out for Musos-->
-<div class="boxed">
-
-<img src="TCMC/Musos.PNG" width="160" height="202" alt=""/>
-<table class="CallMusos">
-    <thead>
-            	<tr>
-                	<th colspan="5">Call out for Musos</th>
-                    
-                </tr>
-            </thead>
-            <tbody>
- 				<td>Calling all Muso’s, entertainers, performers. We are looking to book performers/musicians/entertainers for a family friendly festival at the Ingham Tyto Wetlands on the 23rd of May. This inaugural annual event is a family fun day in the parklands and we are looking for appropriate acts. We are looking for a wide variety of bands and entertainers from our local and surrounding areas. <br>
-<br>
-Deadline for initial submissions 29 March 2015. 
-Artists will need to be insured for public liability.
-<br>
-<br>
-If you are interested please email details, examples or links to your work and a quote including travel expenses for performing at the event to the Event Stage Manager Emile Griffiths -
-mutec.audio@gmail.com
-see our Facebook page
-Posted 17-Mar-15  </td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-            </tfoot>
-            </table>
-      </div>
-      
-   <!--Rouvas-->         
-<div class="boxed">
-
-<img src="TCMC/oldMate.PNG" width="186" height="196" alt=""/>
-<table class="Rouvas">
-    <thead>
-            	<tr>
-                	<th colspan="5">Chord Organ Rouvas Academy of Singing </th>
-                    
-                </tr>
-    </thead>
-            <tbody>
-	<td>I have been in the music industry in Sydney for over 30 years in the capacity of singing teacher, stage performer, singer and musician. My daughter Diana Rouvas was a finalist in the 2012 “The Voice” television show and she is a testament to what can be achieved with correct vocal training. Allow me to show you her technique and give yourself the opportunity to excel as a vocalist. Skype lessons also available 
-Visit <a href="rouvasacademyofsinging.com.au">Rouvas Academy of Singing</a>
-or phone John 0404 044 823
-Posted 09-Mar-15  </td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-    </tfoot>
-  </table>
-</div>
-<!--End Rouvas-->
-
- <!--Effortless technique-->         
-<div class="boxed">
-
-<img src="TCMC/ET.PNG" width="247" height="91" alt=""/>
-<table class="ET">
-<thead>
-           	  <tr>
-                	<th colspan="5">Chord Organ Music teaching by Effortless Technique </th>
-                    
-    </tr>
-  </thead>
-            <tbody>
-	<td>Are you struggling to master sections of your pieces at tempo? Are you stressing that you won’t be prepared because you don’t have enough time to practice? Would you like to learn efficient and effective practice skills and strategies to help you play those difficult sections with confidence and ease and to the best of your ability?  
-Visit us on Facebook
-Email Cassandra Fixter
-or phone 0423 626 501
-Posted 17 March 15 </td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-  </tfoot>
-  </table>
-</div>
-
- <!--Meto Opera-->         
-<div class="boxed">
-
-<table class="MO">
-<thead>
-           	  <tr>
-                	<th colspan="5">Metropolitan Opera productions </th>
-                    
-    </tr>
-  </thead>
-            <tbody>
-	<td>The Metropolitan Opera’s award-winning series of operas in HD continues featuring ten operas captured live in high definition direct from New York, and screening concurrently with the NY season. 
-Follow this link for more information: <a href="http://www.sharmillfilms.com.au/?page_id=2195#cinemas ">Sharmill Films</a> <br>
-<br>
-
-Event Cinemas are considering bringing the Met Opera HD series to Townsville. If you would like to encourage them to go ahead, please email Alexis at <a href="mailto:support@eventcinemas.helpserve.com">support@eventcinemas.helpserve.com</a> with a simple message such as, ‘Please bring Met Opera to Townsville’. You may of course add additional comments if you wish. 
-
-
-Should you decide to contact Alexis, the Music Centre would appreciate receiving a copy of your email, as it will help us assess the level of support from the Townsville arts community. Please copy your<a href="mailto: email to admin@townsvillemusic.org.au ">email to admin@townsvillemusic.org.au</a> 
-
-
-We invite you to forward this message to others who may be interested.</td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-  </tfoot>
-  </table>
-</div>
-
-
- <!--Office admin-->         
-<div class="boxed">
-
-<table class="office">
-<thead>
-           	  <tr>
-                	<th colspan="5">Office Administrator Required! </th>
-                    
-    </tr>
-  </thead>
-            <tbody>
-	<td>The Townsville Community Music Centre is getting busier all the time, and we need a part-time Office Administrator. 
-This is a long-term, part time, part-paid position (all conditions are negotiable). 
-Experience with not-for profit organisations would be an advantage. 
-We use MYOB and Microsoft Office, mostly Word and Excel. 
-For more information -
-<a href="mailto:admin@townsvillemusic.org.au">Email Us</a>
-or phone Jeff 4724 2086
-Posted 15-March-2015</td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-  </tfoot>
-  </table>
-</div>
-
- <!--Dear fellow musos-->         
-<div class="boxed">
-
-<table class="DearFellowMusos">
-<thead>
-           	  <tr>
-                	<th colspan="5">Dear Fellow Musos </th>
-                    
-    </tr>
-  </thead>
-            <tbody>
-	<td>I sing and play keyboard (when I have one). 
-Blues/jazz songs mostly.
-<a href="mailto:knowles_ada@yahoo.com.au">Please Email Ada Knowles</a>-</td>
-
-				<td>
-                <button type="button">Edit</button> </td>
-                <td> <button type="button">Remove</button></td>
-                                           
-            </tbody>
-  <tfoot>
-  </tfoot>
-  </table>
-</div>
+If you would like to take part, please call Merle Trembath 0418882633 anytime. </p> 
+<p>Posted 24-Mar-15</p>
+				
+                <button type="button">Edit</button> 
+                <button type="button">Remove</button>                                    
+       
+			</div>
                 
                 <!-- -->
             </div>
