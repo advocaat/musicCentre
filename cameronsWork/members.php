496d49570f8cf90645bbb29e6685d07ac523a72e
Daan97 <daan.feltonbusch@my.jcu.edu.au>
2015-05-21 15:55:08 +1000

    update members html

--- cameronsWork/members.php
+++ cameronsWork/members.php
@@ -24,6 +24,19 @@
         <div class="row">
             <div id="main" class="col wide">
                 <h2>Members</h2>
+          
+            <!--HTML Main Content-->
+            
+            <div class="Members">                	                                                    
+               <p> You can support the Music Centre by becoming a Member and derive some benefits for yourself at the same time. Your subscription helps to keep us operating and we provide substantial discounts whenever possible.</p> 
+
+             <p>For the Music Centre's own events, Members' ticket discounts can be as high as 50%!</p>
+
+            <p>The Music Centre is also registered as a Deductible Gift Recipient. Any extra donations are tax-deductible! </p> 
+            <p>To become a Member, or to update an existing Membership, complete the application form below or email:<br>
+            <a href="mailto:admin@townsvillemusic.org.au">Townsville Music Centre</a><br>
+	                                                   
+            </div>
             </div>
             <div class="col narrow">
                 <!-- login module -->
